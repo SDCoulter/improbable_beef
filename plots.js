89c9f2f931905eb04c8e1ca2d83ddcdd2e8d3f4f
Shaun Coulter <shaundcoulter@gmail.com>
2021-05-03 21:25:36 -0400

    added dynamic data lookup and population of html file

--- plots.js
+++ plots.js
@@ -1,32 +1,45 @@
 function init() {
-  data = [{
-    x: [1, 2, 3, 4, 5],
-    y: [1, 2, 4, 8, 16] }];
-  Plotly.newPlot("plot", data);
+  var selector = d3.select("#selDataset");
+
+  d3.json("samples.json").then((data) => {
+    console.log(data);
+    var sampleNames = data.names;
+    sampleNames.forEach((sample) => {
+      selector
+          .append("option")
+          .text(sample)
+          .property("value", sample);
+    });
+  });
 };
 
-d3.selectAll("#dropdownMenu").on("change", updatePlotly);
+function optionChanged(newSample) {
+  buildMetadata(newSample);
+  buildCharts(newSample);
+};
 
-function updatePlotly() {
-  var dropdownMenu = d3.select("#dropdownMenu");
-  var dataset = dropdownMenu.property("value");
+function buildMetadata(sample) {
+  d3.json("samples.json").then((data) => {
+    // Pull all the metadata from the JSON.
+    var metadata = data.metadata;
+    // Return only the data with the matching ID number.
+    var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+    // Pull the object from the array.
+    var result = resultArray[0];
+    // Find the div with the matching id.
+    var PANEL = d3.select("#sample-metadata");
 
-  var xData = [1, 2, 3, 4, 5];
-  var yData = [];
-
-  if (dataset === 'dataset1') {
-    yData = [1, 2, 4, 8, 16];
-  };
-
-  if (dataset === 'dataset2') {
-    yData = [1, 10, 100, 1000, 10000];
-  };
-
-  var trace = {
-    x: [xData],
-    y: [yData],
-  };
-  Plotly.restyle("plot", trace);
+    // Clear anything that is in the div already.
+    PANEL.html("");
+    // Append h6 elements with the metadata.
+    PANEL.append("h5").text("ID: " + result.id);
+    PANEL.append("h5").text("ETHNICITY: " + result.ethnicity);
+    PANEL.append("h5").text("GENDER: " + result.gender);
+    PANEL.append("h5").text("AGE: " + result.age);
+    PANEL.append("h5").text("LOCATION: " + result.location);
+    PANEL.append("h5").text("BBTYPE: " + result.bbtype);
+    PANEL.append("h5").text("WFREQ: " + result.wfreq);
+  });
 };
 
 init();
